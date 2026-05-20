468ca74fe16ded34c4620a3aa1f6541bf3058edb
Nuray Lara AÇAR <l45097721@gmail.com>
2026-05-21 00:05:56 +0300

    Update ai_service.py

--- Backend/ai_service.py
+++ Backend/ai_service.py
@@ -10,6 +10,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:5500",
+        "https://nuraylaraacar-seng.github.io",
         "null"
     ],
     allow_credentials=True,
